ad3be398f596411049ff5bcbe0d197367efe3675
Ratin Gao <ratin@knsoft.org>
2023-11-29 20:07:21 +0800

    [NTDLL] Add some NtWow64* functions

--- Source/Include/Wintexports/Undoc_API_Ntdll.h
+++ Source/Include/Wintexports/Undoc_API_Ntdll.h
@@ -594,6 +594,55 @@ NtSetEvent(
 
 #pragma endregion Event
 
+#pragma region Wow64
+
+/* Caution: Exported from Wow Ntdll.dll only!!! */
+
+_Must_inspect_result_
+__drv_allocatesMem(Mem)
+__kernel_entry NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtWow64AllocateVirtualMemory64(
+    _In_ HANDLE ProcessHandle,
+    _Inout_ _At_(*BaseAddress, _Readable_bytes_(*RegionSize) _Writable_bytes_(*RegionSize) _Post_readable_byte_size_(*RegionSize)) PULONGLONG BaseAddress,
+    _In_ ULONGLONG ZeroBits,
+    _Inout_ PULONGLONG RegionSize,
+    _In_ ULONG AllocationType,
+    _In_ ULONG Protect);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtWow64QueryInformationProcess64(
+    _In_ HANDLE ProcessHandle,
+    _In_ PROCESSINFOCLASS ProcessInformationClass,
+    _Out_writes_bytes_to_(ProcessInformationLength, *ReturnLength) PVOID ProcessInformation,
+    _In_ ULONG ProcessInformationLength,
+    _Out_opt_ PULONG ReturnLength);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtWow64ReadVirtualMemory64(
+    _In_ HANDLE ProcessHandle,
+    _In_ ULONGLONG BaseAddress,
+    _Out_ PVOID Buffer,
+    _In_ ULONGLONG NumberOfBytesToRead,
+    _Out_opt_ PULONGLONG NumberOfBytesRead);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtWow64WriteVirtualMemory64(
+    _In_ HANDLE ProcessHandle,
+    _In_ ULONGLONG BaseAddress,
+    _In_ PVOID Buffer,
+    _In_ ULONGLONG NumberOfBytesToWrite,
+    _Out_opt_ ULONGLONG NumberOfBytesWritten);
+
+#pragma endregion Wow64
+
 NTSYSAPI
 NTSTATUS
 NTAPI
